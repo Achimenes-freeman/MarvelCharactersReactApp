cc610542d8b389ed3b2bfdd5f45db02e7a6afead
Achimenes-freeman <gangsta10ip@mail.ru>
2022-04-21 13:26:38 +0300

    Реализация фокуса и активного состояния элементов в блоке списка персонажей

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,4 @@
 import { Component } from 'react/cjs/react.production.min';
-import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 
 import Spinner from '../spinner/Spinner';
